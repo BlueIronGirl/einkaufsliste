b37583ca65e76a398f6f67664cc5fbc73e41153c
Alice Bedow <alice.bedow@gmail.com>
2023-07-27 18:33:20 +0200

    Einkaufsliste - NGRX

--- projekte/einkaufsliste/frontend/src/app/app.module.ts
+++ projekte/einkaufsliste/frontend/src/app/app.module.ts
@@ -1,9 +1,12 @@
-import { NgModule } from '@angular/core';
+import { NgModule, isDevMode } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { EinkaufszettelComponent } from './components/einkaufszettel/einkaufszettel.component';
+import { StoreModule } from '@ngrx/store';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { EffectsModule } from '@ngrx/effects';
 
 @NgModule({
   declarations: [
@@ -12,7 +15,10 @@ import { EinkaufszettelComponent } from './components/einkaufszettel/einkaufszet
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    StoreModule.forRoot({}, {}),
+    StoreDevtoolsModule.instrument({ maxAge: 25, logOnly: !isDevMode() }),
+    EffectsModule.forRoot([])
   ],
   providers: [],
   bootstrap: [AppComponent]
