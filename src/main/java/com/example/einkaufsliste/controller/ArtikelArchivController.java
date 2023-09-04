48ec620c5c8d60ed1548020dc5b0a9683d9767ed
Alice Bedow <alice.bedow@gmail.com>
2023-09-04 21:54:58 +0200

    Docker

--- src/main/java/com/example/einkaufsliste/controller/ArtikelArchivController.java
+++ src/main/java/com/example/einkaufsliste/controller/ArtikelArchivController.java
@@ -4,6 +4,7 @@ import com.example.einkaufsliste.entity.Artikel;
 import com.example.einkaufsliste.entity.ArtikelArchiv;
 import com.example.einkaufsliste.service.ArtikelArchivService;
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
